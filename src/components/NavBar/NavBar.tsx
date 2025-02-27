d5779dbe0c3a12aff2ef42c17ab4a5334093cd3f
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-02-27 15:50:56 +0530

    initial NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,30 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <div className="pi-xora-logo">
+                <div className="pi-xora-text">piXora</div>
+                <img className="pi-xora-icon"
+                    src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/reswr-1.png"
+                    anima-src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/reswr-1.png"
+                    alt="reswr 1"/>
+            </div>
+            {/*NavBar Buttons*/}
+            <div className="NavBar-Buttons">
+                <div className="marketplace-button inter-bold-white-24px">Marketplace</div>
+                <div className="live-auctions-button inter-bold-white-24px">Live Auctions</div>
+                <div className="digital-arts-button inter-bold-white-24px">Digital Arts</div>
+                <div className="photographs-button inter-bold-white-24px">Photographs</div>
+            </div>
+            <div className="profile-button">
+                <img className="user-icon"
+                     src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/user-square-rounded.svg"
+                     anima-src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/user-square-rounded.svg"
+                     alt="user-square-rounded"/>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
