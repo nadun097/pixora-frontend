ee1cc05b5f40f0cdc2731133229b3e5030f855b9
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-02-27 15:51:20 +0530

    Added HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import './HomePage.css'
+// import { Link } from 'react-router-dom'
+import NavBar from '../../components/NavBar/NavBar'
+
+function HomePage() {
+    return (
+        <>
+            {/*import Navbar*/}
+            <NavBar></NavBar>
+        </>
+    )
+}
+
+export default HomePage
