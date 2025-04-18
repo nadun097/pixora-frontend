b223932c6294322de5cc1754d37d4812574d79dd
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-04-18 14:32:38 +0530

    Refactor NavBar component: update logo image, add login button, and enhance styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,13 +4,7 @@ import './NavBar.css'
 function NavBar() {
     return (
         <div className="NavBar">
-            <div className="pi-xora-logo">
-                <div className="pi-xora-text">piXora</div>
-                <img className="pi-xora-icon"
-                    src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/reswr-1.png"
-                    anima-src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/reswr-1.png"
-                    alt="reswr 1" />
-            </div>
+            <img src="src/assets/images/pixoraLogo.png" className="pi-xora-icon" />
             {/*NavBar Buttons*/}
             <div className="NavBar-Buttons">
                 <div className="nav-button nav-button-marketplace-button inter-bold-white-24px">Marketplace</div>
@@ -20,9 +14,11 @@ function NavBar() {
             </div>
 
             <div className="profile-button">
-                <img className="user-icon"
-                    src="https://cdn.animaapp.com/projects/67ac3ff7b09554a0b38573b8/releases/67ac401ef81239f3d08a9f30/img/user-square-rounded.svg"
-                    alt="user-square-rounded" />
+                    <button className="login-button">
+                        <div className="overlap-group">
+                            <div className="text-wrapper">Login</div>
+                        </div>
+                    </button>
             </div>
         </div>
     )
