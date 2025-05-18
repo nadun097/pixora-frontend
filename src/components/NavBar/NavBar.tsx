d53179a4067ea6c6c868cb5d3da9c1f3d86c0bed
AmodMatheesha2003 <amodmathisha@gmail.com>
2025-05-18 14:57:38 +0530

    add login/logout functionality with localStorage token persistence

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,57 +3,47 @@ import styles from "./NavBar.module.css";
 import UserIconDropdown from "../DropDown/UserIcondd";
 
 type NavBarProps = {
-  isLoggedIn: boolean;
-  onLoginClick: () => void;
+    isLoggedIn: boolean;
+    onLoginClick: () => void;
+    onLogout: () => void;
 };
 
-function NavBar({ onLoginClick, isLoggedIn }: NavBarProps) {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+function NavBar({ onLoginClick, isLoggedIn, onLogout }: NavBarProps) {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const handleDropDownClick = () => {
-    setIsDropdownOpen((prev) => !prev);
-  };
+    const handleDropDownClick = () => setIsDropdownOpen((prev) => !prev);
+    const handleDropdownClose = () => setIsDropdownOpen(false);
 
-  const handleDropdownClose = () => {
-    setIsDropdownOpen(false);
-  };
+    const handleHomeClick = () => (window.location.href = "/");
+    const handleMarketplaceClick = () => (window.location.href = "/marketplace");
+    const handleDigitalArtsClick = () => (window.location.href = "/digitalatrs");
 
- 
-
-  const handleHomeClick = () => {
-    window.location.href = "/";
-  };
-   const handleMarketplaceClick = () => {
-    window.location.href = "/marketplace";
-  };
-
-   const handleDigitalArtsClick = () => {
-    window.location.href = "/digitalatrs";
-  };
-  return (
-    <div className={styles.NavBar}>
-      <img src="src/assets/images/pixoraLogo.png" className={styles.pixoraIcon} alt="Pixora Logo" />
-      <div className={styles.NavBarButtons}>
-        <div className={styles.NavButton} onClick={handleHomeClick}>Home</div>
-        <div className={styles.NavButton} onClick={handleMarketplaceClick}>Marketplace</div>
-        <div className={styles.NavButton}>Live Auctions</div>
-        <div className={styles.NavButton} onClick={handleDigitalArtsClick}>Digital Arts</div>
-        <div className={styles.NavButton}>Photographs</div>
-      </div>
-      {!isLoggedIn ? (
-        <div className={styles.profileContainer}>
-          <button className={styles.profileButton} onClick={handleDropDownClick}>
-            <img src="src/assets/images/User icon.png" className={styles.UserIcon} alt="Profile" />
-          </button>
-          {isDropdownOpen && <UserIconDropdown onClose={handleDropdownClose} />}
+    return (
+        <div className={styles.NavBar}>
+            <img src="src/assets/images/pixoraLogo.png" className={styles.pixoraIcon} alt="Pixora Logo" />
+            <div className={styles.NavBarButtons}>
+                <div className={styles.NavButton} onClick={handleHomeClick}>Home</div>
+                <div className={styles.NavButton} onClick={handleMarketplaceClick}>Marketplace</div>
+                <div className={styles.NavButton}>Live Auctions</div>
+                <div className={styles.NavButton} onClick={handleDigitalArtsClick}>Digital Arts</div>
+                <div className={styles.NavButton}>Photographs</div>
+            </div>
+            {!isLoggedIn ? (
+                <button className={styles.loginButton} onClick={onLoginClick}>
+                    <div className={styles.textWrapper}>Login</div>
+                </button>
+            ) : (
+                <div className={styles.profileContainer}>
+                    <button className={styles.profileButton} onClick={handleDropDownClick}>
+                        <img src="src/assets/images/User icon.png" className={styles.UserIcon} alt="Profile" />
+                    </button>
+                    {isDropdownOpen && (
+                        <UserIconDropdown onClose={handleDropdownClose} onLogout={onLogout} />
+                    )}
+                </div>
+            )}
         </div>
-      ) : (
-        <button className={styles.loginButton} onClick={onLoginClick}>
-                <div className={styles.textWrapper}>Login</div>
-            </button> 
-      )}
-    </div>
-  );
+    );
 }
 
 export default NavBar;
