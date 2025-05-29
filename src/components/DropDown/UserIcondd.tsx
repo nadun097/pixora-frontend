d22a082eb3d404eacff31aafdf45e6165febdc68
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-05-29 23:39:14 +0530

    Rename UserIcondd.css to UserIcondd.module.css and update class names for CSS modules

--- src/components/DropDown/UserIcondd.tsx
+++ src/components/DropDown/UserIcondd.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './UserIcondd.css';
+import styles from './UserIcondd.module.css';
 import imgProfile from '../../assets/images/sia croven.jpg';
 import fallbackImg from '../../assets/images/no_user2.png';
 import { useNavigate } from "react-router-dom";
@@ -15,7 +15,7 @@ const UserIconDropdown: React.FC<{ onClose: () => void; onLogout: () => void }> 
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-            const dropdown = document.querySelector('.dropdown-menu');
+            const dropdown = document.querySelector('.dropdown_menu');
             if (dropdown && !dropdown.contains(event.target as Node)) {
                 handleClose();
             }
@@ -25,46 +25,50 @@ const UserIconDropdown: React.FC<{ onClose: () => void; onLogout: () => void }> 
     }, []);
 
     return (
-        <div className={`dropdown-menu ${isHiding ? 'fade-out' : 'fade-in'}`}>
-            <div className="user-profile">
-                <img
-                    src={imgProfile}
-                    alt="preview"
-                    className="profile-image"
-                    onError={(e) => { e.currentTarget.src = fallbackImg; }}
-                />
-                <p className="dropdown-toggle">sia croven</p>
+        <div className={`${styles.dropdown_menu} ${isHiding ? styles.fade_out : styles.fade_in}`}>
+            <div className={styles.user_profile}>
+                <img src={imgProfile} alt="preview" className={styles.profile_image} onError={(e) => { e.currentTarget.src = fallbackImg; }}/>
+                <p className={styles.dropdown_toggle}>sia croven</p>
             </div>
 
-            <div className="dropdown-section">
-                <button
-                    className="dropdown-item"
-                    onClick={() => {
-                        navigate("/userAccount");
-                        onClose();
-                    }}
-                >
-                    <span className="dropdown-icon gray-icon"><img src="src/assets/images/user.png" alt="user" /></span> Your profile
+            <div className={styles.dropdown_section}>
+                <button className={styles.dropdown_item} onClick={() => {navigate("/userAccount");onClose();}}>
+                    <span className={`${styles.dropdown_icon} ${styles.gray_icon}`}>
+                        <img src="src/assets/images/user.png" alt="user" />
+                    </span>
+                    Your profile
                 </button>
 
-                <button className="dropdown-item">
-                    <span className="dropdown-icon gray-icon"><img src="src/assets/images/nft icon.png" alt="preview" /></span> Your NFTs
+                <button className={styles.dropdown_item}>
+                    <span className={`${styles.dropdown_icon} ${styles.gray_icon}`}>
+                        <img src="src/assets/images/nft icon.png" alt="preview" />
+                    </span>
+                    Your NFTs
                 </button>
 
-                <button className="dropdown-item">
-                    <span className="dropdown-icon gray-icon"><img src="src/assets/images/cart .png" alt="shopping cart" /></span> You Bought
-                </button>
-            </div>
-
-            <div className="dropdown-section">
-                <button className="dropdown-item"   onClick={() => {handleClose(); navigate('/dashboard');}}>
-                    <span className="gray-icon"><img src="src/assets/images/application.png" alt="preview" /></span> Dashboard
+                <button className={styles.dropdown_item}>
+                    <span className={`${styles.dropdown_icon} ${styles.gray_icon}`}>
+                        <img src="src/assets/images/cart .png" alt="shopping cart" />
+                    </span>
+                    You Bought
                 </button>
             </div>
 
-            <div className="dropdown-section">
-                <button className="dropdown-button" onClick={onLogout}>
-                    <span className="red-icon"><img src="src/assets/images/sign out option.png" alt="sign out" /></span> Sign out
+            <div className={styles.dropdown_section}>
+                <button className={styles.dropdown_item}   onClick={() => {handleClose(); navigate('/dashboard');}}>
+                    <span className={styles.gray_icon}>
+                        <img src="src/assets/images/application.png" alt="preview"/>
+                    </span>
+                    Dashboard
+                </button>
+            </div>
+
+            <div className={styles.dropdown_section}>
+                <button className={styles.dropdown_button} onClick={onLogout}>
+                    <span className={styles.red_icon}>
+                        <img src="src/assets/images/sign out option.png" alt="sign out"/>
+                    </span>
+                    Sign out
                 </button>
             </div>
         </div>
